4e6ea5818b9b3d93e0611e29fa43fa3fa9ee6da6
Yiran Sheng <shengyiran@gmail.com>
2017-03-30 15:41:24 -0700

    refactoring quite a bit, and add some features

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 export { read, dispatch, effect, end, rollback } from "./dsl";
 
-export { Do } from "./free";
+export { isFree, Do } from "./free";
 
 import enhancer from "./enhancer";
 export default enhancer;
