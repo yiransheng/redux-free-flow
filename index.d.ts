39d3d70b6593e6de4ff10500fc84ddbc6be34635
Yiran Sheng <shengyiran@gmail.com>
2019-06-16 17:37:13 +0800

    Export Do typing, even weak

--- index.d.ts
+++ index.d.ts
@@ -1,34 +1,97 @@
 import { Action, AnyAction, Store, StoreEnhancer, Dispatch } from "redux";
 
-declare const freeM: unique symbol;
+export interface FreeDSL<S, A, T> {
+  map<U>(f: (x: T) => U): _Map<U, { [indirect]: ThisType<_UnionAll<S, A, T>> }>;
 
-interface FreeDSL<S, A, T> {
-  [freeM]: true;
+  then<M extends FreeDSL<S, A, unknown>>(
+    f: (x: T) => M
+  ): _AndThen<ValueType<M>, { [indirect]: ThisType<_UnionAll<S, A, T>> }>;
 
-  map<U>(f: (x: T) => U): FreeDSL<S, A, U>;
-
-  then<U>(f: (x: T) => FreeDSL<S, A, U>): FreeDSL<S, A, U>;
-
-  andThen<U>(f: (x: T) => FreeDSL<S, A, U>): FreeDSL<S, A, U>;
-
-  phantom<Ss, As>(this: FreeDSL<unknown, unknown, T>): FreeDSL<Ss, Aa>;
+  andThen<M extends FreeDSL<S, A, unknown>>(
+    f: (x: T) => M
+  ): _AndThen<ValueType<M>, { [indirect]: ThisType<_UnionAll<S, A, T>> }>;
 }
 
-export declare function read<S, A, T>(f: (state: S) => T): FreeDSL<S, A, T>;
+declare const __read__: unique symbol;
 
-export declare function dispatch<S, A>(action: A): FreeDSL<S, A, void>;
+declare const __effect__: unique symbol;
 
-export declare function effect<S, A, T>(
-  factory: () => Promise<T>
-): FreeDSL<S, A, T>;
+declare const __dispatch__: unique symbol;
 
-export declare const end: FreeDSL<any, any, void>;
+declare const __end__: unique symbol;
 
-export declare const rollback: FreeDSL<any, any, void>;
+declare const __rollback__: unique symbol;
+
+declare const __and_then__: unique symbol;
+
+declare const __map__: unique symbol;
+
+declare const indirect: unique symbol;
+
+interface _Read<S, T> extends FreeDSL<S, unknown, T> {
+  [__read__]: true;
+}
+
+interface _Dispatch<A> extends FreeDSL<unknown, A, void> {
+  [__dispatch__]: true;
+}
+
+interface _Effect<T> extends FreeDSL<unknown, unknown, T> {
+  [__effect__]: true;
+}
+
+interface _End extends FreeDSL<unknown, unknown, void> {
+  [__end__]: true;
+}
+
+interface _Rollback extends FreeDSL<unknown, unknown, void> {
+  [__rollback__]: true;
+}
+
+interface _AndThen<T, F extends { [indirect]: any }>
+  extends FreeDSL<
+    StateType<F[typeof indirect]>,
+    ActionType<F[typeof indirect]>,
+    T
+  > {
+  [__and_then__]: true;
+}
+
+interface _Map<T, F extends { [indirect]: any }>
+  extends FreeDSL<
+    StateType<F[typeof indirect]>,
+    ActionType<F[typeof indirect]>,
+    T
+  > {
+  [__map__]: true;
+}
+
+type _UnionAll<S, A, T> =
+  | _Read<S, T>
+  | _Dispatch<A>
+  | _Effect<T>
+  | _Rollback
+  | _End
+  | _AndThen<T, { [indirect]: _UnionAll<S, A, any> }>;
+
+type StateType<M> = M extends FreeDSL<infer S, unknown, unknown> ? S : never;
+
+type ActionType<M> = M extends FreeDSL<unknown, infer A, unknown> ? A : never;
+
+type ValueType<M> = M extends FreeDSL<unknown, unknown, infer T> ? T : never;
+
+export declare function read<S, T>(f: (state: S) => T): _Read<S, T>;
+
+export declare function dispatch<A>(action: A): _Dispatch<A>;
+
+export declare function effect<T>(factory: () => Promise<T>): _Effect<T>;
+
+export declare const end: _End;
+
+export declare const rollback: _Rollback;
 
 export declare function isFree<S, A, T>(val: any): val is FreeDSL<S, A, T>;
 
-// generator Do notation loses pretty much all type safety, unfortunately
 export declare function Do<S, A, T>(
   generator: () => IterableIterator<FreeDSL<unknown, unknown, unknown>>
 ): FreeDSL<S, A, T>;
